d73ac2d570ee076359eb63cf34214cdec1f850cd
Michael <mihail.tchernov@yandex.ru>
2021-07-13 09:52:29 +0300

    Add support command line parameters

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from datetime import datetime
 import logging
+import argparse
 
 import asyncio
 import aiofiles
@@ -38,5 +39,21 @@ async def save_messages(host: str, port: int, output_file: str):
 
 
 if __name__ == '__main__':
-    logging.basicConfig(level=logging.DEBUG)
-    asyncio.run(save_messages('minechat.dvmn.org', 5000, 'messages.txt'))
+    parser = argparse.ArgumentParser(description='Minechat listener')
+    parser.add_argument('--host', type=str, help='server host')
+    parser.add_argument('--port', type=int, help='server port number')
+    parser.add_argument('--out', type=str,
+                        help='output path message history file')
+    parser.add_argument('--debug', type=str,
+                        help='turn on/off logger. Values - ON/OFF')
+    args = parser.parse_args()
+
+    if not args.host or not args.port or not args.out:
+        logging.error('required parameters not found')
+
+    if args.debug == 'ON':
+        logging.basicConfig(level=logging.DEBUG)
+    else:
+        logging.basicConfig(level=logging.ERROR)
+
+    asyncio.run(save_messages(args.host, args.port, args.out))
