f884d7eb1554bfe154f3f001428aa85f2e9b7dd1
Michael <mihail.tchernov@yandex.ru>
2021-07-13 09:16:17 +0300

    Save messages history to file

--- main.py
+++ main.py
@@ -1,21 +1,42 @@
-import asyncio
+from datetime import datetime
 import logging
 
+import asyncio
+import aiofiles
 
-async def save_messages(host: str, port: int, output_file: str,
-                        messages_count=10):
-    reader, writer = await asyncio.open_connection(host, port)
-    with open(output_file, 'w') as f:
-        for i in range(messages_count):
-            logging.info(f'Reading message #{i + 1}...')
+
+def message_formatting(message: str) -> str:
+    current_datetime = datetime.now().strftime('%d.%m.%y %H:%M')
+    return f'[{current_datetime}] {message}'
+
+
+async def save_messages(host: str, port: int, output_file: str):
+    try:
+        reader, writer = await asyncio.open_connection(host, port)
+    except ConnectionRefusedError:
+        logging.error('Host is unreachable')
+        return
+
+    async with aiofiles.open(output_file, 'a') as file_handle:
+        message = message_formatting('Установлено соединение')
+        logging.debug(message)
+        await file_handle.write(message+'\n')
+
+        while True:
             message = await reader.readline()
-            message = message.decode().rstrip()
-            f.write(message+'\n')
-            logging.info(f'Message text: {message}')
+            try:
+                message = message.decode().rstrip()
+            except UnicodeDecodeError:
+                break
+
+            message = message_formatting(message)
+            await file_handle.write(message + '\n')
+            await file_handle.flush()
+            logging.debug(message)
     writer.close()
     await writer.wait_closed()
 
 
 if __name__ == '__main__':
     logging.basicConfig(level=logging.DEBUG)
-    asyncio.run(save_messages('minechat.dvmn.org', 5000, 'messages.txt', 2))
+    asyncio.run(save_messages('minechat.dvmn.org', 5000, 'messages.txt'))
