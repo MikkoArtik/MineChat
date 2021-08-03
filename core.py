f7fe744cfbdfb7e6d2561839c74173257e9f2c8d
Michael <mihail.tchernov@yandex.ru>
2021-08-03 13:03:04 +0300

    Add CLI options to sender script

--- core.py
+++ core.py
@@ -1,31 +1,19 @@
-import os
 import json
 import logging
 
 import asyncio
 from asyncio import StreamReader, StreamWriter
 
-import dotenv
-
 EMPTY_LINE = '\n'
 MAX_MESSAGE_BYTE_SIZE = 1024
 
 
-def change_env_file(hash_value: str):
-    if not os.path.exists('.env'):
-        with open('.env', 'w') as f:
-            f.close()
-    dotenv.load_dotenv()
-    dotenv.set_key('.env', 'TOKEN', hash_value)
-    logging.debug('Hash-код сохранен в файл')
-
-
-async def get_connection(host_addr: str, port: int):
+async def get_connection(host_address: str, port: int):
     try:
-        reader, writer = await asyncio.open_connection(host_addr, port)
+        reader, writer = await asyncio.open_connection(host_address, port)
         return reader, writer
     except ConnectionRefusedError:
-        logging.error('Ошибка соединения')
+        logging.error('Error of connection')
         return
 
 
@@ -44,15 +32,16 @@ async def register(reader: StreamReader, writer: StreamWriter, nickname: str):
     user_info = await reader.readline()
     user_info = json.loads(user_info.decode())
 
-    logging.debug(f'Hash-код аккаунта: {user_info["account_hash"]}')
-    change_env_file(user_info['account_hash'])
+    logging.debug(f'Account token: {user_info["account_hash"]}')
 
     writer.close()
     await writer.wait_closed()
-    logging.debug('Соединение закрыто')
+    logging.debug('Connection was closed')
+    return user_info['account_hash']
 
 
-async def authorise(reader: StreamReader, writer: StreamWriter, hash_key: str):
+async def is_authorise(reader: StreamReader, writer: StreamWriter,
+                       hash_key: str):
     await reader.read(MAX_MESSAGE_BYTE_SIZE)
 
     hash_key += '\n'
@@ -62,11 +51,12 @@ async def authorise(reader: StreamReader, writer: StreamWriter, hash_key: str):
     hash_info = await reader.readline()
     hash_info = hash_info.decode().rstrip()
     if json.loads(hash_info) is None:
-        logging.error(
-            'Неверный токен. Проверьте его или зарегистрируйте заново')
+        logging.error('Invalid token. Check token or register new')
         writer.close()
         await writer.wait_closed()
-        logging.debug('Соединение закрыто')
+        logging.debug('Connection was closed')
+        return False
+    return True
 
 
 async def submit_message(reader: StreamReader, writer: StreamWriter,
---
+++ main.py
@@ -0,0 +1,60 @@
+import os
+import argparse
+import asyncio
+
+import dotenv
+
+from core import get_connection, register, is_authorise, submit_message
+
+
+DEFAULT_HOST = 'minechat.dvmn.org'
+DEFAULT_PORT = 5050
+
+
+async def main():
+    parser = argparse.ArgumentParser(
+        description='Utility for sending messages to server')
+    parser.add_argument('--host', type=str, help='Server address')
+    parser.add_argument('--port', type=int, help='Server port')
+    parser.add_argument('--token', type=str, help='User token')
+    parser.add_argument('--nick', type=str, help='New user nickname')
+    parser.add_argument('message', type=str, help='Message text')
+
+    arguments = parser.parse_args()
+
+    if arguments.host:
+        host = arguments.host
+    else:
+        host = DEFAULT_HOST
+
+    if arguments.port:
+        port = arguments.port
+    else:
+        port = DEFAULT_PORT
+
+    connection = await get_connection(host, port)
+    if not connection:
+        return
+    reader, writer = connection
+
+    if arguments.nick:
+        token = await register(reader, writer, arguments.nick)
+        connection = await get_connection(host, port)
+        if not connection:
+            return
+        reader, writer = connection
+    elif arguments.token:
+        token = arguments.token
+    else:
+        dotenv.load_dotenv()
+        token = os.getenv('HOST')
+
+    is_auth = await is_authorise(reader, writer, token)
+    if is_auth:
+        await submit_message(reader, writer, arguments.message)
+    writer.close()
+    await writer.wait_closed()
+
+
+if __name__ == '__main__':
+    asyncio.run(main())
